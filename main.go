1ea2248459964b9a30b19135b469121bcc7ef37c
Ademu Anthony <ademuanthony@gmail.com>
2022-03-21 02:23:25 +0400

    allow all origin

--- main.go
+++ main.go
@@ -102,7 +102,7 @@ func _main(ctx context.Context) error {
 
 func CORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Add("Access-Control-Allow-Origin", "*")
 		// w.Header().Add("Access-Control-Allow-Credentials", "true")
 		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
