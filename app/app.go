05531907c72e65d8912ef878b1488356693d9a87
Ademu Anthony <ademuanthony@gmail.com>
2022-03-27 22:26:15 +0400

    key for pkc mod

--- app/app.go
+++ app/app.go
@@ -79,8 +79,8 @@ func (m module) buildRoute() {
 	// PACKAGES
 	m.server.AddRoute("/api/packages/list", web.GET, m.GetPackages)
 	m.server.AddRoute("/api/packages/get", web.GET, m.GetPackage)
-	m.server.AddRoute("/api/packages/create", web.POST, m.CreatePackage, m.server.RequireLogin)
-	m.server.AddRoute("/api/packages/update", web.POST, m.UpdatePackage, m.server.RequireLogin)
+	m.server.AddRoute("/api/packages/create", web.POST, m.CreatePackage, m.server.RequireLogin, m.server.ValidAPIKey)
+	m.server.AddRoute("/api/packages/update", web.POST, m.UpdatePackage, m.server.RequireLogin, m.server.ValidAPIKey)
 	m.server.AddRoute("/api/packages/buy", web.POST, m.BuyPackage, m.server.RequireLogin, m.server.NoReentry)
 	m.server.AddRoute("/api/packages/subscription", web.GET, m.GetActiveSubscription, m.server.RequireLogin)
 
