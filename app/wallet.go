6925a1fbbb20912332dd55a80f2e2d69a6719d38
Ademu Anthony <ademuanthony@gmail.com>
2022-07-07 21:07:01 +0100

    inc fee

--- app/wallet.go
+++ app/wallet.go
@@ -83,7 +83,7 @@ func (m module) transfer(ctx context.Context, privateKeyStr, to string, value *b
 	// 	return "", err
 	// }
 
-	feeStr := "0.00000002"
+	feeStr := "0.000000021"
 	feeFloat, err := ParseBigFloat(feeStr)
 	if err != nil {
 		return "", err
