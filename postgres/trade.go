6363e71ded4fdadf579a18aece57e22917eb99b8
Ademu Anthony <ademuanthony@gmail.com>
2022-07-19 16:24:38 +0100

    rep

--- postgres/trade.go
+++ postgres/trade.go
@@ -268,7 +268,7 @@ func (pg PgDb) BuildTradingSchedule(ctx context.Context) error {
 						account.principal > 0 and
 						subscription.start_date <= %d and subscription.end_date >= %d and subscription.package_id = '%s'
 			`
-			pDivisor := 30 * p.TradesPerDay * 2
+			pDivisor := 30 * p.TradesPerDay * 4
 			if _, err := models.TradeSchedules(
 				qm.SQL(fmt.Sprintf(statement,
 					tradeNo,
